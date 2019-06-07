1aa9e4d5e1ed2b10bcebc7e3f6f3b25fa73da01c
Pin-Cheng Shih <pincheng0101@gmail.com>
2019-06-07 23:31:02 +0800

    Optimization

--- eat_apples.py
+++ eat_apples.py
@@ -9,18 +9,21 @@ ready =  driver.find_element_by_id("ready-btn")
 ready.click()
 
 while True:
-    i = 0
-    max_index = 0
-    max_location = 0
+
     try:
         score_button = driver.find_elements_by_xpath("//div[(contains(@class, 't1') or contains(@class, 't2') or contains(@class, 't3') or contains(@class, 't4') or contains(@class, 't5')) and not(contains(@class, 'tt'))]")
-        for x in score_button:
-            if x.location['y'] > max_location:
-                max_location = x.location['y']
-                max_index = i
-            i+=1
-        score_button[max_index].click()
-        #webdriver.ActionChains(driver).move_to_element(score_button[max_index]).click().perform()
+        for i in range(len(score_button)):
+            i = 0
+            max_index = 0
+            max_location = 0
+            for x in score_button:
+                if x.location['y'] > max_location:
+                    max_location = x.location['y']
+                    max_index = i
+                i+=1
+            score_button[max_index].click()
+            score_button.pop(max_index)
+            #webdriver.ActionChains(driver).move_to_element(score_button[max_index]).click().perform()
     except Exception as e:
         print (e)
         pass
