33c7e4a201ae1fc9db0a445b82f4e3e50fec2bef
Pin-Cheng Shih <pincheng0101@gmail.com>
2019-06-07 23:38:03 +0800

    Remove unnecessary code

--- eat_apples.py
+++ eat_apples.py
@@ -1,15 +1,13 @@
 from selenium import webdriver
 import time
-import re
 
 driver = webdriver.Chrome()
 driver.get("http://game.hg0355.com/game/xpg/?from=timeline&isappinstalled=0")
 time.sleep(0.5)
-ready =  driver.find_element_by_id("ready-btn")
+ready = driver.find_element_by_id("ready-btn")
 ready.click()
 
 while True:
-
     try:
         score_button = driver.find_elements_by_xpath("//div[(contains(@class, 't1') or contains(@class, 't2') or contains(@class, 't3') or contains(@class, 't4') or contains(@class, 't5')) and not(contains(@class, 'tt'))]")
         for i in range(len(score_button)):
