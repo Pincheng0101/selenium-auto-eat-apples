9fec0eb70e9125e4f05b1489c1111fb8b7828b5c
Pin-Cheng Shih <pincheng0101@gmail.com>
2019-06-07 16:33:50 +0800

    Replace regex igonre pattern with not contains

--- eat_apples.py
+++ eat_apples.py
@@ -8,18 +8,16 @@ time.sleep(0.5)
 ready =  driver.find_element_by_id("ready-btn")
 ready.click()
 
-ignorePattern = re.compile("tt")
 while True:
+    i = 0
+    max_index = 0
+    max_location = 0
     try:
-        score_button = driver.find_elements_by_xpath("//div[contains(@class, 't1') or contains(@class, 't2') or contains(@class, 't3') or contains(@class, 't4') or contains(@class, 't5')]")
-        i = 0
-        max_index = 0
-        max_location = 0
+        score_button = driver.find_elements_by_xpath("//div[(contains(@class, 't1') or contains(@class, 't2') or contains(@class, 't3') or contains(@class, 't4') or contains(@class, 't5')) and not(contains(@class, 'tt'))]")
         for x in score_button:
-            if not ignorePattern.search(x.get_attribute("class")):
-                if x.location['y'] > max_location:
-                    max_location = x.location['y']
-                    max_index = i
+            if x.location['y'] > max_location:
+                max_location = x.location['y']
+                max_index = i
             i+=1
         score_button[max_index].click()
         #webdriver.ActionChains(driver).move_to_element(score_button[max_index]).click().perform()
